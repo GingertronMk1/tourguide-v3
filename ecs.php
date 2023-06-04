36b35ce64588b34d480accb913d0a3f5558066e4
Jack Ellis <jackellis1504@gmail.com>
2023-06-04 14:12:32 +0100

    Don't even look at the bootstrap folder

--- ecs.php
+++ ecs.php
@@ -9,7 +9,6 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 return function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
         __DIR__ . '/config',
         __DIR__ . '/public',
         __DIR__ . '/resources',
